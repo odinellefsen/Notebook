d5fdafdfb761e8d32c364dffba17ead63bb0331c
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2021-09-20 09:40:06 +0100

    javastuff

---
+++ Initial-Page/index.js
@@ -0,0 +1,11 @@
+fn1 = () => {
+    if (document.getElementById('navnid') === 'Óðin Andor Ellefsen') {
+        <a href="notebook.html"></a>
+    }
+    else if(document.getElementById('navnid') === 'Baldur Kári Ellefsen') {
+        <a href="dnote.html"></a>
+    }
+    else {
+        alert('Hello')
+    }
+}

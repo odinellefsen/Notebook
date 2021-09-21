9d5af1680a91e9236ab3ba17f05b864633ce7509
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2021-09-21 14:12:24 +0100

    jea

--- Initial-Page/index.js
+++ Initial-Page/index.js
@@ -1,3 +1,24 @@
-let navn = document.getElementById('navnid')
+/* fn1 = () => {
+    if(document.getElementById('navnid') === 'Light Yagami') {
+    location.replace('/Death-Note/dnote.html')
+    }
+}
 
-if( navn === 'Óðin Andor Ellefsen') location.replace('/notebook.html')
+*/
+Notebook = () => {
+
+    let searchText = document.getElementById("form-search").value;
+    let form = document.getElementById('myForm');
+  
+    if(searchText === 'Light Yagami') {
+      document.getElementById("s").value = searchText;
+      form.action = "/Death-Note/dnote.html";
+      form.submit();
+    } else if (searchText === 'Óðin Andor Ellefsen' || 'Óðin A. Ellefsen' || 'Óðin A Ellefsen' || 'Óðin Ellefsen' || 'Odin Andor Ellefsen' || 'Odin Ellefsen') {
+      document.getElementById("s").value = searchText;
+      form.action = "/Notebook/notebook.html";
+      form.submit();  
+    } else {
+        return false;
+    }
+  }

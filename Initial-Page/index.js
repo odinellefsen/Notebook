990fb5a5c4e5f26cc40fa1c1a6101fecf50271be
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2021-09-21 10:09:27 +0100

    hello

--- Initial-Page/index.js
+++ Initial-Page/index.js
@@ -1,11 +1,3 @@
-fn1 = () => {
-    if (document.getElementById('navnid') === 'Óðin Andor Ellefsen') {
-        <a href="notebook.html"></a>
-    }
-    else if(document.getElementById('navnid') === 'Baldur Kári Ellefsen') {
-        <a href="dnote.html"></a>
-    }
-    else {
-        alert('Hello')
-    }
-}
+let navn = document.getElementById('navnid')
+
+if( navn === 'Óðin Andor Ellefsen') location.replace('/notebook.html')
